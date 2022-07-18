c44b1d02959045993690428b5b363cff862b8680
WonWoo-Shin <93998008+WonWoo-Shin@users.noreply.github.com>
2022-07-18 18:39:55 +0900

    Add files via upload
    add logout

--- JS/greeting.js
+++ JS/greeting.js
@@ -1,28 +1,40 @@
 const loginForm = document.querySelector("#loginForm");
 const loginInput = document.querySelector("#loginForm input");
-const greeting = document.querySelector("#greeting")
+const greeting = document.querySelector("#greeting");
 const HIDDEN_CLASS = "hidden";
 const USERNAME_KEY = "username";
 
-function paintGreeting(name){
-    greeting.classList.remove(HIDDEN_CLASS);
-    greeting.innerText = `Hello ${name}`;  
+function logoutFunc() {
+  greeting.classList.add(HIDDEN_CLASS);
+  loginForm.classList.remove(HIDDEN_CLASS);
+  localStorage.removeItem(USERNAME_KEY);
+  loginInput.value = "";
+  loginForm.addEventListener("submit", loginSubmit);
 }
 
-function loginSubmit(event){
-    event.preventDefault();
-    const username = loginInput.value;
-    localStorage.setItem(USERNAME_KEY,username);
-    loginForm.classList.add(HIDDEN_CLASS);
-    paintGreeting(username);
+function paintGreeting(name) {
+  greeting.classList.remove(HIDDEN_CLASS);
+  greeting.innerText = `Hello ${name}`;
+  const logout = document.createElement("button");
+  logout.id = "logout";
+  logout.innerText = "✖";
+  logout.addEventListener("click", logoutFunc);
+  greeting.appendChild(logout);
+}
+
+function loginSubmit(event) {
+  event.preventDefault();
+  const username = loginInput.value;
+  localStorage.setItem(USERNAME_KEY, username);
+  loginForm.classList.add(HIDDEN_CLASS);
+  paintGreeting(username);
 }
 
 const savedUsername = localStorage.getItem(USERNAME_KEY);
 
-if(savedUsername===null){
-    loginForm.classList.remove(HIDDEN_CLASS);
-    loginForm.addEventListener("submit",loginSubmit);
-}
-else{
-    paintGreeting(savedUsername);
+if (savedUsername === null) {
+  loginForm.classList.remove(HIDDEN_CLASS);
+  loginForm.addEventListener("submit", loginSubmit);
+} else {
+  paintGreeting(savedUsername);
 }
